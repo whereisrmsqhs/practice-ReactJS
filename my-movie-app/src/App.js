5e0fb3136c3bcf68097a7cf645a00453d4977270
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-03-07 01:59:44 +0900

    feat, navbar impl

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/Main";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
       </Routes>
---
+++ my-movie-app/src/components/Header.js
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClapperboard } from "@fortawesome/free-solid-svg-icons";
+import "../css/header.css";
+
+function Header() {
+  return (
+    <nav>
+      <div>
+        <FontAwesomeIcon
+          icon={faClapperboard}
+          size={"2xl"}
+          style={{ color: "#FFD43B" }}
+          bounce
+        />
+        <span className="title">Nomad Movie</span>
+      </div>
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Quick Search"
+      ></input>
+      <div className="navbar-nav">
+        <ul className="navbar-nav-list">
+          <li>Home</li>
+          <li>4K</li>
+          <li>Trending</li>
+          <li>Browser Movies</li>
+          <li>
+            <strong>Login</strong>
+          </li>
+          <li>
+            <strong>Register</strong>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
