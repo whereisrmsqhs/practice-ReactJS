3bbc9fb1dc06790c10a3c31f93d48fdcdc36e55c
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-03-12 00:08:02 +0900

    pause, infinite scroll function stop because of lack of knowledge of sync/async and react hooks

--- my-movie-app/src/routes/Main.js
+++ my-movie-app/src/routes/Main.js
@@ -1,58 +1,54 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Movies from "../components/Movies";
 import Filter from "../components/Filter";
 import "../css/main.scss";
+
+const useIntersect = (onIntersectFn, options) => {
+  const ref = useRef(null); // 1
+  const callback = useCallback(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) onIntersectFn(entry, observer);
+      });
+    },
+    [onIntersectFn]
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(callback, options); // 2
+    observer.observe(ref.current); // 3
+    return () => observer.disconnect();
+  }, [ref, options, callback]);
+
+  return ref;
+};
 
 function Main() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
-  let imageRef = useRef(null);
+  const ref = useIntersect(
+    async (entry, observer) => {
+      observer.unobserve(entry.target);
+      getMovies(page);
+    },
+    { threshold: 0.5 } // Adjust the threshold value as needed
+  );
 
-  const getMovies = async () => {
+  const getMovies = async (tpage) => {
     const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?page=${page}`
+      `https://yts.mx/api/v2/list_movies.json?page=${tpage}`
     );
     const json = await response.json();
     setMovies((preMovies) => [...preMovies, ...json.data.movies]);
     setLoading(false);
-    console.log(imageRef);
-  };
-
-  const handleIntersection = (entries) => {
-    const entry = entries[0];
-    if (entry.isIntersecting) {
-      setPage((prePage) => prePage + 1);
-    }
+    // setPage((page) => page + 1);
   };
 
   useEffect(() => {
     getMovies();
   }, [page]);
-
-  useEffect(() => {
-    console.log("무한 루프 로직 돌아가는중");
-
-    // imageRef가 null인 경우, 렌더링 시에 이미지가 아직 로딩되지 않았을 수 있습니다.
-    if (!imageRef.current) {
-      console.log("imageRef is null. Waiting for image to load...");
-      return;
-    }
-
-    console.log(imageRef.current);
-
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5,
-    });
-
-    if (imageRef.current) {
-      observer.observe(imageRef.current);
-    }
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [imageRef, page]);
 
   return (
     <div>
@@ -61,7 +57,7 @@ function Main() {
       ) : (
         <div>
           {/* <Filter /> */}
-          <div className="image_container" ref={imageRef}>
+          <div className="image_container">
             {movies.map((movie) => (
               <Movies
                 key={movie.id}
@@ -71,6 +67,7 @@ function Main() {
                 rating={movie.rating}
               />
             ))}
+            <div ref={ref} style={{ width: "100%", height: 30 }} />
           </div>
         </div>
       )}
