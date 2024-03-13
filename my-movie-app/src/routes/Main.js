adff4df669ec7a9d1f81fee525c6a0be4a494717
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-03-13 13:48:02 +0900

    fix, infinite scroll issue

--- my-movie-app/src/routes/Main.js
+++ my-movie-app/src/routes/Main.js
@@ -1,50 +1,52 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useCallback, useState, useMemo } from "react";
 import Movies from "../components/Movies";
 import Filter from "../components/Filter";
 import "../css/main.scss";
 
-const useIntersect = (onIntersectFn, options) => {
-  const ref = useRef(null); // 1
-  const callback = useCallback(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) onIntersectFn(entry, observer);
-      });
-    },
-    [onIntersectFn]
+const movieFetch = async (page) =>
+  await fetch(`https://yts.mx/api/v2/list_movies.json?page=${page}`).then(
+    async (res) => await res.json()
   );
-
-  useEffect(() => {
-    if (!ref.current) return;
-    const observer = new IntersectionObserver(callback, options); // 2
-    observer.observe(ref.current); // 3
-    return () => observer.disconnect();
-  }, [ref, options, callback]);
-
-  return ref;
-};
 
 function Main() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
-  const ref = useIntersect(
-    async (entry, observer) => {
-      observer.unobserve(entry.target);
-      getMovies(page);
-    },
-    { threshold: 0.5 } // Adjust the threshold value as needed
+
+  const getMovies = () => {
+    setLoading(true);
+
+    movieFetch(page).then(({ data }) => {
+      console.log("movieFetch done");
+      setMovies((preMovies) => [...preMovies, ...data.movies]);
+      setLoading(false);
+      console.log("Movie" + page);
+    });
+  };
+  const handleIntersection = useCallback((entries) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      setPage((prePage) => prePage + 1);
+    }
+  }, []);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(handleIntersection, {
+        rootMargin: "300px",
+      }),
+    []
   );
 
-  const getMovies = async (tpage) => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?page=${tpage}`
-    );
-    const json = await response.json();
-    setMovies((preMovies) => [...preMovies, ...json.data.movies]);
-    setLoading(false);
-    // setPage((page) => page + 1);
-  };
+  const imageRef = useCallback(
+    (element) => {
+      if (!element) return;
+      observer.observe(element);
+    },
+    [observer]
+  );
+
+  useEffect(() => () => observer.disconnect(), []);
 
   useEffect(() => {
     getMovies();
@@ -52,25 +54,35 @@ function Main() {
 
   return (
     <div>
-      {loading ? (
-        "Loading..."
-      ) : (
-        <div>
-          {/* <Filter /> */}
-          <div className="image_container">
-            {movies.map((movie) => (
-              <Movies
-                key={movie.id}
-                id={movie.id}
-                coverImg={movie.medium_cover_image}
-                genre={movie.genres}
-                rating={movie.rating}
-              />
-            ))}
-            <div ref={ref} style={{ width: "100%", height: 30 }} />
-          </div>
+      <div>
+        <Filter />
+        <div className="image_container">
+          {movies.map((movie, index) => (
+            <Movies
+              key={index}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              genre={movie.genres}
+              rating={movie.rating}
+            />
+          ))}
         </div>
-      )}
+        {loading && (
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              "justify-content": "center",
+              "align-items": "center",
+              "padding-top": "24px",
+              "padding-bottom": "24px",
+            }}
+          >
+            Loading...
+          </div>
+        )}
+        <div ref={imageRef} />
+      </div>
     </div>
   );
 }
